03c3f4fddc1dfb6fc0a41d7ecc3654dfc958eb74
1203217 <jorgina.arres@est.fib.upc.edu>
2020-04-13 22:45:44 +0200

    Use request performer

--- src/AgentClient.java
+++ src/AgentClient.java
@@ -17,59 +17,64 @@ public class AgentClient extends Agent {
     protected void setup() {
 
         System.out.println("Hallo! Buyer-agent "+ getAID().getName() +" is ready.");
-        addBehaviour(new SimpleBehaviour() {
-            private boolean first = true;
-            @Override
-            public void action() {
-                ACLMessage proposal = receive();
-                if (proposal!=null && proposal.getPerformative()==ACLMessage.PROPOSE) {
-                    ACLMessage accept = proposal.createReply();
-                    accept.setPerformative(ACLMessage.ACCEPT_PROPOSAL);
-                    System.out.println("Client Agent: Yes, I want");
-                }
-                if (first) {
-                    ACLMessage msg = new ACLMessage(ACLMessage.QUERY_IF);
-                    msg.addReceiver(new AID("RestaurantGatekeeperAgent", AID.ISLOCALNAME));
-                    msg.setLanguage("English");
-                    msg.setOntology("Reservation-Restaurant-Ontology");
-                    msg.setContent("Can I book a 2 people table to have dinner on Saturday at 20:00h?");
-                    send(msg);
-                    first=false;
-                }
-            }
 
-            @Override
-            public boolean done() {
-                return false;
-            }
-        });
         Object[] args = getArguments();
-        if (args != null && args.length > 0)
-        {
-            CustomerRestaurantWantedType ="Spanish";
-            System.out.println( "Trying to buy " +args[0]);
+        if (args != null && args.length > 0) {
+            CustomerRestaurantWantedType = "Spanish";
+            System.out.println("Trying to buy " + args[0]);
+            addBehaviour(new TickerBehaviour(this, 10000) {
 
+                protected void onTick() {
+                    // Update the list of agents
+                    System.out.println("Ticker behaviour:");
+                    DFAgentDescription template = new DFAgentDescription();
+                    ServiceDescription sd = new ServiceDescription();
+                    sd.setType("reserving");
+                    template.addServices(sd);
+                    try {
+                        DFAgentDescription[] result = DFService.search(myAgent, template);
+                        AID[] tempAgents = new AID[result.length];
+                        System.out.println("AgentsNames:");
+                        for (int i = 0; i < result.length; ++i) {
+                            tempAgents[i] = result[i].getName();
+                            System.out.println(result[i].getName());
+
+                        }
+                    } catch (FIPAException fe) {
+                        fe.printStackTrace();
+                    }
+                    // Perform the request
+                    myAgent.addBehaviour(new RequestPerformer());
+                }
+            });
         }
-        addBehaviour(new TickerBehaviour(this, 10000) {
-            protected void onTick() {
-                // Update the list of agents
-                System.out.println("Ticker behaviour:");
-                DFAgentDescription template = new DFAgentDescription();
-                ServiceDescription sd = new ServiceDescription();
-                sd.setType("reserving");
-                template.addServices(sd);
-                try {
-                    DFAgentDescription[ ] result = DFService.search(myAgent, template);
-                    AID[] tempAgents = new AID[result.length];
-                    System.out.println("AgentsNames:");
-                    for (int i = 0; i < result.length; ++i) {
-                        tempAgents[i] = result[i].getName();
-                        System.out.println(result[i].getName());
 
-                    } }
-                catch (FIPAException fe) {
-                    fe.printStackTrace(); }
-                // Perform the request myAgent.addBehaviour(new RequestPerformer()); }
-            } });
+    }
+    class RequestPerformer extends Behaviour {
+        private boolean first = true;
 
-}}
+        @Override
+        public void action() {
+            ACLMessage proposal = receive();
+            if (proposal!=null && proposal.getPerformative()==ACLMessage.PROPOSE) {
+                ACLMessage accept = proposal.createReply();
+                accept.setPerformative(ACLMessage.ACCEPT_PROPOSAL);
+                System.out.println("Client Agent: Yes, I want");
+            }
+            if (first) {
+                ACLMessage msg = new ACLMessage(ACLMessage.QUERY_IF);
+                msg.addReceiver(new AID("RestaurantGatekeeperAgent", AID.ISLOCALNAME));
+                msg.setLanguage("English");
+                msg.setOntology("Reservation-Restaurant-Ontology");
+                msg.setContent("Can I book a 2 people table to have dinner on Saturday at 20:00h?");
+                send(msg);
+                first=false;
+            }
+        }
+
+        @Override
+        public boolean done() {
+            return false;
+        }
+    }
+}
